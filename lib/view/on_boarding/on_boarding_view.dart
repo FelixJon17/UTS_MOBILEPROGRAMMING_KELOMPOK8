9cb1031386b89ef581e77534bcd22792071eec89
Borjues <dhani.535230149@stu.untar.ac.id>
2024-09-16 22:29:43 +0700

    Main.dart

--- lib/view/on_boarding/on_boarding_view.dart
+++ lib/view/on_boarding/on_boarding_view.dart
@@ -10,6 +10,6 @@ class OnBoardingView extends StatefulWidget {
 class _OnBoardingViewState extends State<OnBoardingView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold();
   }
 }
