0d6dcbc2d828589ce02b1e098b3ab92d712c42ea
FelixFerdinad <felixferdinad3@gmail.com>
2024-09-17 22:57:05 +0700

    tambahkan file common, commong_widget, dan view

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:fitnessapp/view/on_boarding/on_boarding_view.dart';
+import 'package:fitnessapp/view/view/on_boarding_view.dart';
 import 'package:flutter/material.dart';
 import 'common/colo_extension.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,26 +15,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Fitness App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        primaryColor: TColor.primaryColor1,
-        fontFamily: "Poppins"
-      ),
+          // This is the theme of your application.
+          //
+          // TRY THIS: Try running your application with "flutter run". You'll see
+          // the application has a purple toolbar. Then, without quitting the app,
+          // try changing the seedColor in the colorScheme below to Colors.green
+          // and then invoke "hot reload" (save your changes or press the "hot
+          // reload" button in a Flutter-supported IDE, or press "r" if you used
+          // the command line to start the app).
+          //
+          // Notice that the counter didn't reset back to zero; the application
+          // state is not lost during the reload. To reset the state, use hot
+          // restart instead.
+          //
+          // This works for code too, not just values: Most code changes can be
+          // tested with just a hot reload.
+          primaryColor: TColor.primaryColor1,
+          fontFamily: "Poppins"),
       home: const OnBoardingView(),
     );
   }
 }
-
