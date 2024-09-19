78e8af5411478385a888fb68ccda2e8e3477e6a3
FelixJon17 <felixjonathan2665@gmail.com>
2024-09-19 08:19:11 +0700

    add funfacts

--- lib/screen/fun_fact_screen.dart
+++ lib/screen/fun_fact_screen.dart
@@ -23,7 +23,9 @@ class FunFactsScreen extends StatelessWidget {
     '2. Maintain proper form during exercises to target the right muscles.',
     '3. Stay hydrated before, during, and after your workouts.',
     '4. Incorporate rest days in your routine to allow muscle recovery.',
-    '5. Gradually increase weights to avoid straining your muscles and joints.'
+    '5. Gradually increase weights to avoid straining your muscles and joints.',
+    '6. Music improves workout performance',
+    '7. Running 10 minutes per miles can burn 104 calories per miles'
   ];
 
   @override
